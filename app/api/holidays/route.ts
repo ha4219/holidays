63049fb7b41c72d866fa8473d394e7754b31d7b0
ha4219 <ha4219@naver.com>
2023-12-18 18:30:05 +0900

    update

--- app/api/holidays/route.ts
+++ app/api/holidays/route.ts
@@ -12,33 +12,43 @@ export async function GET(req: Request) {
             status: 400,
         })
     }
-    if (start.slice(0, 4) != end.slice(0, 4)) {
-        return new Response(HTTP_STATUS_400_YEAR_MATCH, {
-            status: 400,
-        })
+    // if (start.slice(0, 4) != end.slice(0, 4)) {
+    //     return new Response(HTTP_STATUS_400_YEAR_MATCH, {
+    //         status: 400,
+    //     })
+    // }
+    if (new Date(start).getTime() > new Date(end).getTime()) {
+        return new Response(JSON.stringify({
+            holidays: [],
+        }))
     }
-    try {
-        var url = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo'; /*URL*/
-        var queryParams = '?' + 'servicekey=' + process.env.SERVICE_KEY; /*Service Key*/
-        queryParams += '&' + encodeURIComponent('solYear') + '=' + start.slice(0,4); /**/
-        queryParams += '&numOfRows=100'
 
+    try {
         const ret: string[] = [];
-        const st = new Date(start);
-        const en = new Date(end);
-        const res = await fetch(url + queryParams).then(res => res.text()).then(res => xml2js.parseStringPromise(res));
-        // console.log(res.response.body[0].items[0].item)
-        for(const item of res.response.body[0].items[0].item) {
-            const val = item.locdate[0].replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
-            const date = new Date(val);
-            if (st.getTime() > date.getTime()) {
-                continue;
+
+        for(let year = Number(start.slice(0, 4)); year <= Number(end.slice(0, 4)); year ++) {
+            var url = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo'; /*URL*/
+            var queryParams = '?' + 'servicekey=' + process.env.SERVICE_KEY; /*Service Key*/
+            queryParams += '&' + encodeURIComponent('solYear') + '=' + String(year); /**/
+            queryParams += '&numOfRows=100'
+
+            const st = new Date(start);
+            const en = new Date(end);
+            const res = await fetch(url + queryParams).then(res => res.text()).then(res => xml2js.parseStringPromise(res));
+            // console.log(res.response.body[0].items[0].item)
+            for(const item of res.response.body[0].items[0].item) {
+                const val = item.locdate[0].replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3');
+                const date = new Date(val);
+                if (st.getTime() > date.getTime()) {
+                    continue;
+                }
+                if (date.getTime() > en.getTime()) {
+                    break;
+                }
+                ret.push(val);
             }
-            if (date.getTime() > en.getTime()) {
-                break;
-            }
-            ret.push(val);
         }
+        
 
         return new Response(JSON.stringify({holidays: ret}));
     } catch(e: any) {
